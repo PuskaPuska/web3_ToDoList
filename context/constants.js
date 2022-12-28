aee3723c6baa0f712aef2c765f0b177361fa1fc0
PuskaPuska <k0novi@yandex.ru>
2022-12-28 12:27:03 +0300

    second commit

---
+++ context/constants.js
@@ -0,0 +1,4 @@
+import ToDoList from "./ToDoList.json";
+
+export const toDolistAddress = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266";
+export const toDolistAddressABI = ToDoList.abi;
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+//INTERNAL IMPORT
+import { VotingProvider } from "../context/ToDoList";
+
+const MyApp = ({ Component, pageProps }) => (
+  <VotingProvider>
+    <div>
+      <Component {...pageProps} />
+    </div>
+  </VotingProvider>
+);
+
+export default MyApp;
